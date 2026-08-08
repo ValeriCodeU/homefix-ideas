495729e3f7adecf8da5f1b2e57a4653c0abfb826
ValeriCodeU <valeri.yanev@icloud.com>
2026-08-08 12:48:43 +0300

    Add authService for user register, login and logout

---
+++ client/src/services/authService.js
@@ -0,0 +1,12 @@
+import request from './request';
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const register = (email, password) =>
+    request(`${baseUrl}/register`, 'POST', { email, password });
+
+export const login = (email, password) =>
+    request(`${baseUrl}/login`, 'POST', { email, password });
+
+export const logout = () =>
+    request(`${baseUrl}/logout`, 'GET');
